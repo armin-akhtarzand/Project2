af914ec63f5fe7dd203ce07a75beaad6ff244043
armin-akhtarzand <armin.akhtarzand@outlook.com>
2025-10-08 23:19:27 +0200

    Added ConsoleUI, changes the UI from JOptionPane to console if chosen

--- Lab2/src/main/java/se/iths/armin/project2/Application.java
+++ Lab2/src/main/java/se/iths/armin/project2/Application.java
@@ -3,6 +3,7 @@ package se.iths.armin.project2;
 import se.iths.armin.project2.controller.Service;
 import se.iths.armin.project2.controller.Storage;
 import se.iths.armin.project2.model.Product;
+import se.iths.armin.project2.view.ConsoleUI;
 import se.iths.armin.project2.view.DialogUI;
 
 import java.util.List;
@@ -13,14 +14,15 @@ public class Application {
 
         Storage storage = new Storage();
         List<Product> products = storage.load();
-        DialogUI ui = new DialogUI();
-        Service service = new Service(ui, products);
+        DialogUI diaUi = new DialogUI();
+        ConsoleUI consUI = new ConsoleUI();
 
-        boolean exit = false;
+        //Service service = new Service(consUI, products);
+        Service service = new Service(diaUi, products);
 
 
         while (true) {
-            String choice = ui.menu("Main menu", List.of("Add new product", "List all products", "Show product info", "Exit program"));
+            String choice = diaUi.menu("Main menu", List.of("Add new product", "List all products", "Show product info", "Exit program"));
             if (choice == null) {
                 return;
             }
